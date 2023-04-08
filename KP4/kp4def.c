7c262ca9a5cdc27e64e79cffbe9f14864f57e1e3
Mihail Kaluyzhnyy <pifagerodot@gmail.com>
2023-04-08 15:51:01 +0300

    20

---
+++ KP4/kp4def.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+void sqrtx(int x);
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    sqrtx(x);
+    return 0;
+}
+
+void sqrtx(int x) {
+    int a = x / 2 + 1;
+    int c = x;
+    int b;
+    while (a * a != x) {
+        b = a;
+        if (a * a < x) {
+            a = a + (c - a) / 2;
+        }
+        else {
+            c = a;
+            a = a / 2 + 1;
+        }
+        printf("%d\n", a);
+        if (a == b) {
+            printf("%d\n", a);
+            break;
+        }
+    }
+}
